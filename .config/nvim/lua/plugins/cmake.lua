d3ed2898b28539494d8bfcb8785f8b86a8f6964c
Rafal Jasinski <rajs@softwaremind.com>
2025-09-30 14:33:36 +0200

    Updated the cmake tools options

--- .config/nvim/lua/plugins/cmake.lua
+++ .config/nvim/lua/plugins/cmake.lua
@@ -1,4 +1,21 @@
 return {
   "Civitasv/cmake-tools.nvim",
-  opts = {},
+  dependencies = {
+    "akinsho/toggleterm.nvim",
+  },
+  opts = {
+    cmake_regenerate_on_save = false,
+    cmake_dap_configuration = {
+      type = "cppdbg",
+    },
+    cmake_executor = {
+      name = "toggleterm",
+      opts = {
+        split_direction = "vertical",
+      },
+    },
+    cmake_runner = {
+      name = "toggleterm",
+    },
+  },
 }
