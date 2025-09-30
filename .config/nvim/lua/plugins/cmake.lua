7a91b967d445c5bd9b38c207debef2bdd3d69df2
Rafal Jasinski <rajs@softwaremind.com>
2025-09-30 16:23:35 +0200

    Removed virtual text from cmake projects

--- .config/nvim/lua/plugins/cmake.lua
+++ .config/nvim/lua/plugins/cmake.lua
@@ -2,6 +2,7 @@ return {
   "Civitasv/cmake-tools.nvim",
   opts = {
     cmake_regenerate_on_save = false,
+    cmake_virtual_text_support = false,
     cmake_dap_configuration = {
       type = "cppdbg",
     },
