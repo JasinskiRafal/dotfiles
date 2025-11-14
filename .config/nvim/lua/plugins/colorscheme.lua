fd343b29b70c0a96cce9d42590260c7e03a8ca84
Rafal Jasinski <rajs@softwaremind.com>
2025-11-14 16:05:48 +0100

    Fixed gruvbox

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
     opts = {},
     init = function()
       vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_float_style = "none"
+      vim.g.gruvbox_material_float_style = "blend"
       vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
       vim.cmd.colorscheme("gruvbox-material")
     end,
