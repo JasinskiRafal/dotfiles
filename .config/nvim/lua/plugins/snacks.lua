b458741e557e7739ce2dd63a209b8ca260eddc24
Rafal Jasinski <rajs@softwaremind.com>
2026-02-03 12:05:22 +0100

    Removed unused parts of snacks

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -24,7 +24,6 @@ return {
     explorer = {
       enabled = true,
     },
-    git = { enabled = true },
     indent = {
       enabled = true,
       only_scope = true,
@@ -122,13 +121,6 @@ return {
       desc = "Find Files",
     },
     {
-      "<leader>fg",
-      function()
-        Snacks.picker.git_files()
-      end,
-      desc = "Find Git Files",
-    },
-    {
       "<leader>fp",
       function()
         Snacks.picker.projects()
@@ -141,63 +133,6 @@ return {
         Snacks.picker.recent()
       end,
       desc = "Recent",
-    },
-    -- git
-    {
-      "<leader>gb",
-      function()
-        Snacks.picker.git_branches()
-      end,
-      desc = "Git Branches",
-    },
-    {
-      "<leader>gB",
-      function()
-        Snacks.git.blame_line({ 0 })
-      end,
-      desc = "Git Blame current line",
-    },
-    {
-      "<leader>gl",
-      function()
-        Snacks.picker.git_log()
-      end,
-      desc = "Git Log",
-    },
-    {
-      "<leader>gL",
-      function()
-        Snacks.picker.git_log_line()
-      end,
-      desc = "Git Log Line",
-    },
-    {
-      "<leader>gs",
-      function()
-        Snacks.picker.git_status()
-      end,
-      desc = "Git Status",
-    },
-    {
-      "<leader>gS",
-      function()
-        Snacks.picker.git_stash()
-      end,
-      desc = "Git Stash",
-    },
-    {
-      "<leader>gd",
-      function()
-        Snacks.picker.git_diff()
-      end,
-      desc = "Git Diff (Hunks)",
-    },
-    {
-      "<leader>gf",
-      function()
-        Snacks.picker.git_log_file()
-      end,
-      desc = "Git Log File",
     },
     -- Grep
     {
