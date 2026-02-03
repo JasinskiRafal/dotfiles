4926f088018662d90815410a0a51efb375ca7374
Rafal Jasinski <rajs@softwaremind.com>
2026-02-03 11:57:14 +0100

    Removed unnecessary elements

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -10,28 +10,4 @@ return {
       vim.cmd.colorscheme("gruvbox-material")
     end,
   },
-  {
-    "marko-cerovac/material.nvim",
-    priority = 1000,
-    opts = {},
-    -- init = function()
-    --   vim.g.material_style = "deep ocean"
-    --   vim.cmd.colorscheme("material")
-    -- end,
-  },
-  {
-    "Mofiqul/vscode.nvim",
-    priority = 1000,
-    opts = {
-      style = "dark",
-      italic_comments = true,
-      underline_links = true,
-      color_overrides = {
-        vscCursorDarkDark = "#2d2d2d",
-      },
-    },
-    -- init = function()
-    --   vim.cmd.colorscheme("vscode")
-    -- end,
-  },
 }
--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,18 +1,16 @@
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    opts = { direction = "vertical" , size = vim.o.columns * 0.4},
-    keys = {
-      {
-        "<leader>vt",
-        ":ToggleTerm<CR>",
-        desc = "Toggle terminal",
-      },
-      {
-        "<leader>vT",
-        ":TermNew<CR>",
-        desc = "Open new terminal",
-      },
+  "akinsho/toggleterm.nvim",
+  opts = { direction = "vertical", size = vim.o.columns * 0.4 },
+  keys = {
+    {
+      "<leader>vt",
+      ":ToggleTerm<CR>",
+      desc = "Toggle terminal",
+    },
+    {
+      "<leader>vT",
+      ":TermNew<CR>",
+      desc = "Open new terminal",
     },
   },
 }
